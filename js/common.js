bbdc0d8c110a75e0107acd0bc183874fad2d6e04
slabnevitch <handofschpilrain@mail.ru>
2018-05-04 16:23:13 +0500

    Add files via upload

---
+++ js/common.js
@@ -0,0 +1,64 @@
+$(function() {
+
+
+	$(".toggle-mnu").click(function() {
+		$(this).toggleClass("on");
+		$(".sidebar").toggleClass('active');
+		return false;
+	});
+
+	// Accordeon-----------------------------------
+	$('.acordeon-link').click(function(e) {
+		e.preventDefault();
+		var $currentItem = $(this).closest('.acordeon-item');
+		if($currentItem.hasClass('acordeon-item-with-sublist')){
+			$(this).toggleClass('opened');
+			$currentItem.find('.acordeon-sublist')
+			.toggleClass('opened');
+		}else{
+			return;
+		}
+	});
+// end Accordeon-----------------------------------
+
+	$("form").submit(function() { //Change
+		$.magnificPopup.open({
+			items: {
+				src: $('.buy-popup')
+			},
+			type: 'inline',
+			// mainClass: 'email-modal',
+			callbacks: {
+				beforeOpen: function() {
+					if($(window).width() < 700) {
+						this.st.focus = false;
+					} else {
+						this.st.focus = '#name';
+					}
+					
+				},
+			}
+		});
+		return false;
+	});
+
+	//Chrome Smooth Scroll
+	try {
+		$.browserSelector();
+		if($("html").hasClass("chrome")) {
+			$.smoothScroll();
+		}
+	} catch(err) {
+
+	};
+
+	$("img, a").on("dragstart", function(event) { event.preventDefault(); });
+	
+});
+
+$(window).load(function() {
+
+	$(".loader_inner").fadeOut();
+	$(".loader").delay(400).fadeOut("slow");
+
+});
